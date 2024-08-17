5054752e96f4bfc8a61fd65571959066cc04d1a5
librz <rz19960718@gmail.com>
2024-08-17 17:23:25 +0800

    feat: setup commitlint & commit-msg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const num = Math.ceil(Math.random() * 10);
 
-// eslint rule: curly
+// related eslint rule: curly
 if (num % 2 === 0) {console.log(`${num} is even`);}
 else {console.log(`${num} is odd`);}
